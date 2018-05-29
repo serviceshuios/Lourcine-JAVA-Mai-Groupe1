b2fdd12db02a6968b5ef1a055ef4c03d1de4ca7c
HUIOS <services.huios@gmail.com>
2018-05-29 14:25:13 +0200

    ajout serviceImpl

---
+++ src/service/ServiceImpl.java
@@ -0,0 +1,184 @@
+package service;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import domaine.Club;
+import domaine.Compte;
+import domaine.Login;
+import domaine.Personne;
+
+public class ServiceImpl implements Iservice{
+
+	//DECLARER ET INITIALISER LA LISTE DES PERSONNES
+	private Map<Integer,Personne> personnes = new Hashtable<Integer,Personne>();
+	
+	
+	public Map<Integer, Personne> getPersonnes() {
+		return personnes;
+	}
+
+	public void setPersonnes(Map<Integer, Personne> personnes) {
+		this.personnes = personnes;
+	}
+
+	@Override
+	public Map<Integer,Personne> createPersonne(Personne p) {
+		// TODO Auto-generated method stub
+		personnes.put(p.getIdPersonne(), p);
+		return personnes;
+	}
+
+	@Override
+	public void readPersonne(Personne p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatePersonne(Personne p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePersonne(Personne p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listePersonne(Map<Integer,Personne> personnes) {
+		// TODO Auto-generated method stub
+		for (Map.Entry<Integer, Personne> entry : personnes.entrySet()) {
+			 System.out.println(entry);
+		 }
+	}
+
+	@Override
+	public void affilierClub(Personne p, Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attribuerCompte(Personne p, Compte c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createCompte(Compte c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readCompte(Compte c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCompte(Compte c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCompte(Compte c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listeCompte(Compte c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void crediterCompte(Compte c, double mt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void debiterCompte(Compte c, double mt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ajouterTitulaire(Compte c, Personne p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createClub(Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readClub(Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateClub(Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteClub(Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listeClub(Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ajouterPersonneClub(Personne p, Club c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createLogin(Login l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readLogin(Login l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateLogin(Login l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteLogin(Login l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listeLogin(Login l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
